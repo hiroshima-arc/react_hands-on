b9bb4debd998d7a5c937fb42c11113db7c860681
k2works <kakimomokuri@gmail.com>
2018-08-29 16:56:26 +0900

    Adding a reducer

---
+++ react-app/src/reducers/index.jsx
@@ -0,0 +1,15 @@
+// @flow
+import type { EnthusiasmAction } from '../actions';
+import type { StoreState } from '../types';
+import { INCREMENT_ENTHUSIASM, DECREMENT_ENTHUSIASM } from '../constants';
+
+export default function enthusiasm(state: StoreState, action: EnthusiasmAction): StoreState {
+  switch (action.type) {
+    case INCREMENT_ENTHUSIASM:
+      return { ...state, enthusiasmLevel: state.enthusiasmLevel + 1 };
+    case DECREMENT_ENTHUSIASM:
+      return { ...state, enthusiasmLevel: Math.max(1, state.enthusiasmLevel - 1) };
+    default:
+  }
+  return state;
+}
