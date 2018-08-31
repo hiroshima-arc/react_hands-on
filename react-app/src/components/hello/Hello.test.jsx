2dfa2f1a2925c6ced6b9d9dd21e3f5dec4774c2e
k2works <kakimomokuri@gmail.com>
2018-08-31 11:57:28 +0900

    テストケースの修正

--- react-app/src/components/hello/Hello.test.jsx
+++ react-app/src/components/hello/Hello.test.jsx
@@ -1,19 +1,19 @@
+// @flow
+
 import * as React from 'react';
 import * as enzyme from 'enzyme';
 import Hello from './Hello';
-
-it('renders the correct text when no enthusiasm level is given', () => {
-  const hello = enzyme.shallow(<Hello name="Daniel" />);
-  expect(hello.find('.greeting').text()).toEqual('Hello Daniel!');
-});
+import type { HelloState } from '../../types/hello';
 
 it('renders the correct text with an explicit enthusiasm of 1', () => {
-  const hello = enzyme.shallow(<Hello name="Daniel" enthusiasmLevel={1} />);
+  const initialState: HelloState = { languageName: 'Daniel', enthusiasmLevel: 1 };
+  const hello = enzyme.shallow(<Hello enthusiasm={initialState} />);
   expect(hello.find('.greeting').text()).toEqual('Hello Daniel!');
 });
 
 it('renders the correct text with an expicit enthusiasm level of 5', () => {
-  const hello = enzyme.shallow(<Hello name="Daniel" enthusiasmLevel={5} />);
+  const initialState: HelloState = { languageName: 'Daniel', enthusiasmLevel: 5 };
+  const hello = enzyme.shallow(<Hello enthusiasm={initialState} />);
   expect(hello.find('.greeting').text()).toEqual('Hello Daniel!!!!!');
 });
 
